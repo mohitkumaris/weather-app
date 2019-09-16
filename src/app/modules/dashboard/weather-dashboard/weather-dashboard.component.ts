67e389cd1bde82d4bcd23ba8fde70d3c865c281b
Mohit Kumar <mohitkumaris@gmail.com>
2019-09-16 13:41:37 +0530

    final code

--- src/app/modules/dashboard/weather-dashboard/weather-dashboard.component.ts
+++ src/app/modules/dashboard/weather-dashboard/weather-dashboard.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import { FacadeService } from 'src/app/shared/services/facade.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Subject } from 'rxjs';
 import * as moment from 'moment';
+import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-weather-dashboard',
   templateUrl: './weather-dashboard.component.html',
@@ -10,32 +11,40 @@ import * as moment from 'moment';
 export class WeatherDashboardComponent implements OnInit, OnDestroy {
 
   textValue: string;
-  result: Subscription;
-  weatherData: Array<object>;
+  unSubscribe$ = new  Subject();
+  iResult = [];
   constructor(private facadeService: FacadeService) {}
 
 
   ngOnInit() {}
   ngOnDestroy(): void {
-    this.result.unsubscribe();
+    this.unSubscribe$.next();
+    this.unSubscribe$.complete();
   }
   showWeather(event) {
    const cityName = event.target.value.split(',')[0];
    const countryCode = event.target.value.split(',')[1];
 
-   this.result =  this.facadeService.getWeatherData(cityName, countryCode.toUpperCase()).subscribe((res) => {
-      res.list.filter((f) => {
-     return f.dt_txt.includes('21:00:00');
-    }).map((r) => {
-      return {
-        date: moment(r.dt_txt).format('DD MMMM'),
-      temp_min: r.main.temp_min,
-      temp_max: r.main.temp_max,
-      humidity :  r.main.humidity,
-      weather: r.weather[0].description
-      };
-    });
+  this.facadeService.getWeatherData(cityName, countryCode.toUpperCase())
+   .pipe(
+      takeUntil(this.unSubscribe$)
+   )
+   .subscribe((res: any) => {
+      this.iResult = res.list.filter((f) => {
+        return f.dt_txt.includes('21:00:00');
+       })
+       .map((r) => {
+         return {
+           date: moment(r.dt_txt).format('DD MMMM'),
+         temp_min: r.main.temp_min,
+         temp_max: r.main.temp_max,
+         humidity :  r.main.humidity,
+         weather: r.weather[0].description
+         };
+       });
+
    }, (error) => console.log('Some error in service'));
   }
 }
+
 
