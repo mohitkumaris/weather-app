ec78e3ac2ee1003c1f1487b9721909f003bf2387
Mohit Kumar <mohitkumaris@gmail.com>
2019-10-29 16:06:23 +0530

    test case

--- src/app/modules/dashboard/weather-dashboard/weather-dashboard.component.spec.ts
+++ src/app/modules/dashboard/weather-dashboard/weather-dashboard.component.spec.ts
@@ -1,14 +1,20 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {WeatherDashboardComponent} from './weather-dashboard.component';
+import { InputComponent } from 'src/app/components/input/input.component';
+import { FacadeService } from 'src/app/shared/services/facade.service';
+import { WeatherService } from 'src/app/shared/services/weather.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-describe('WeatherDashboardComponent', () => {
+fdescribe('WeatherDashboardComponent', () => {
   let component: WeatherDashboardComponent;
   let fixture: ComponentFixture<WeatherDashboardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [WeatherDashboardComponent]
+      declarations: [WeatherDashboardComponent, InputComponent],
+      providers: [HttpClient, WeatherService, FacadeService ],
+      imports: [HttpClientModule]
     })
       .compileComponents();
   }));
